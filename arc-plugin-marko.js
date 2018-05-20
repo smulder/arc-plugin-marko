affefbcae5373f6d644877a675a908b91e61a4fa
Scott Mulder <scott@treefortbikes.com>
2018-05-20 07:50:30 -0400

    Allow staticUrlArray for more parallel downloads via subdomains

--- arc-plugin-marko.js
+++ arc-plugin-marko.js
@@ -44,7 +44,7 @@ async function build(config) {
     '"pages" and "config" are required properties to use arc-plugin-marko'
   );
 
-  let { pages, config: buildConfig, bucket, store, staticUrl, bucketDir } = pluginConfig;
+  let { pages, config: buildConfig, bucket, store, staticUrl, staticUrlArray, bucketDir } = pluginConfig;
 
   ok(pages, '"pages" is a required property to use arc-plugin-marko');
 
@@ -89,6 +89,7 @@ async function build(config) {
 		        bucket,
 			   bucketDir,
 			   staticUrl,
+			   staticUrlArray,
 		        logger: console.log
 		      }
 		    });
